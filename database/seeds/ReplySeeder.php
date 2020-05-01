4f7937c2f60c05893cd733062e4e950dd425d1e1
Daniel Sanchez <dansanb@gmail.com>
2020-05-01 08:29:49 -0700

    Implement ReplySeeder class to insert test replies in database

--- database/seeds/ReplySeeder.php
+++ database/seeds/ReplySeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+use Faker\Factory as Faker;
 
 class ReplySeeder extends Seeder
 {
@@ -11,6 +13,16 @@ class ReplySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = Faker::create();
+
+         // create 5000 replies to random topics for testing
+         for ($i=0; $i < 5000; $i++) {           
+            DB::table('replies')->insert([
+                'topic_id' => $faker->numberBetween(1,500),
+                'author_id' => $faker->numberBetween(1,50),
+                'body' => $faker->paragraphs($nb = $faker->numberBetween(1,5), $asText = true),
+                'created_at' => Carbon::now(),
+            ]);
+        }
     }
 }
