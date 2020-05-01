92d3d121a1a15796fdce240b81127b22156f530f
Daniel Sanchez <dansanb@gmail.com>
2020-04-30 23:26:54 -0700

    add empty data seeder class files for forum, topic, and reply tables. add Faker composer dependency to generate fake data.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(ForumSeeder::class);
+        $this->call(TopicSeeder::class);
+        $this->call(ReplySeeder::class);
     }
 }
