cb9d0d915e7380b1a5f4a57484501de79054f8a9
Daniel Sanchez <dansanb@gmail.com>
2020-04-30 23:20:35 -0700

    add 'create_table' migrations for 'forums', 'topics' and 'replies' tables to database.sqlite

---
+++ database/migrations/2020_05_01_061306_create_topics_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTopicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('topics', function (Blueprint $table) {
+            $table->id();
+            $table->integer('forum_id');
+            $table->integer('author_id');
+            $table->string('title', 100);
+            $table->string('body', 1000);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('topics');
+    }
+}
