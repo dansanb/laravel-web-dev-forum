5678f0e07873351eb82a2b2fb3e6612ebd5e9855
Daniel Sanchez <dansanb@gmail.com>
2020-05-01 08:06:43 -0700

    Implement ForumSeeder class to generate  sample forum records.

--- database/seeds/ForumSeeder.php
+++ database/seeds/ForumSeeder.php
@@ -11,6 +11,31 @@ class ForumSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = Faker\Factory::create();
+        
+        DB::table('forums')->insert([
+            'name' => 'PHP',
+            'description' => 'Does it start with an $? Talk about it here!',            
+        ]);
+
+        DB::table('forums')->insert([
+            'name' => 'JavaScript',
+            'description' => "Because it's more than just jQuery.",
+        ]);
+
+        DB::table('forums')->insert([
+            'name' => 'HTML/CSS',
+            'description' => 'What customers and clients really care for.'
+        ]);
+         
+        DB::table('forums')->insert([
+            'name' => 'Databases',
+            'description' => 'Union or Join only spoken here.',
+        ]);
+
+        DB::table('forums')->insert([
+            'name' => 'Servers',
+            'description' => 'We are all root!',
+        ]);
     }
 }
