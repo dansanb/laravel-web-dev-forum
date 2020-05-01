45684d9d47daeaba26ea4c326f044865db77e2f5
Daniel Sanchez <dansanb@gmail.com>
2020-05-01 08:17:55 -0700

    Implemented TopicSeeder class to insert test  topic records in database

--- database/seeds/TopicSeeder.php
+++ database/seeds/TopicSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class TopicSeeder extends Seeder
 {
@@ -11,6 +12,17 @@ class TopicSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = Faker\Factory::create();
+
+        // create 500 topics for testing
+        for ($i=0; $i < 500; $i++) {           
+            DB::table('topics')->insert([
+                'forum_id' => $faker->numberBetween(1,5),
+                'author_id' => $faker->numberBetween(1,50),
+                'title' => $faker->sentence($nbWords = $faker->numberBetween(3,8), $variableNbWords = true),
+                'body' => $faker->paragraphs($nb = $faker->numberBetween(1,5), $asText = true),
+                'created_at' => Carbon::now(),
+            ]);
+        }
     }
 }
